e63e728815a551d8fdc28458fbded88e6dd32491
Sameer Patlekar <166603757+Sameerpatlekar@users.noreply.github.com>
2024-06-26 20:58:04 +0530

    fggj

--- awsrds.tf
+++ awsrds.tf
@@ -41,7 +41,7 @@ resource "aws_db_instance" "example" {
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
   username             = "admin"
-  password             = "admin@123" # Update with a secure password
+  password             = "admin123" # Update with a secure password
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
 
